2c9400badf037bdf33e451956ea1db4060a05fde
alejandrochvs <alejandrochvs@hotmail.com>
2017-09-25 16:29:21 -0500

    Archive validation"

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ $(function () {
         if (e.target != $('#inputFile')[0]) {
             $('#inputFile').click();
         }
+    });
+    $('#inputFile').change(function () {
+        $('#loadStatus').html('Enviar "' + 
+            $(this).val().split('\\').reverse()[0] + '"');
     })
 })
